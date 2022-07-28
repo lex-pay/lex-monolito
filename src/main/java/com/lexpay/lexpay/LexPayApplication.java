c7a7b6859003076b03ec2f557e0369dff4e98532
JhenniferJS <jhennifer@gec.inatel.br>
2022-07-28 20:10:35 -0300

    first commit

---
+++ src/main/java/com/lexpay/lexpay/LexPayApplication.java
@@ -0,0 +1,13 @@
+package com.lexpay.lexpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LexPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lexpay/lexpay/LexPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexpay.lexpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LexPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
